445dc53e80f50419457467644698745b33966bc9
dave-seo <1393193549@qq.com>
2019-11-16 14:13:44 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
+import FastClick from "fastclick";
+FastClick.attach(document.body);
 Vue.config.productionTip = false;
-
+import "amfe-flexible";
+import { Button } from "vant";
+Vue.use(Button);
 new Vue({
   router,
   store,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,13 +2,14 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <van-button type="primary">主要按钮</van-button>
+    <div class="a"></div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "home",
   components: {
@@ -16,3 +17,9 @@ export default {
   }
 };
 </script>
+<style>
+.a {
+  width: 108px;
+  height: 108px;
+}
+</style>
