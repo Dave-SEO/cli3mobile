f7fbbd08e9b548bd2fdd3cd97acf503ea38d141f
dave-seo <1393193549@qq.com>
2019-11-18 17:59:44 +0800

    a

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <van-nav-bar :border="false">
+    <van-nav-bar :border="false" @click-left="onClickLeft">
       <slot slot="left" name="left">
-        <van-icon name="arrow-left" size="20px"/>
+        <van-icon name="arrow-left" size="20px" />
       </slot>
       <slot slot="title" name="title">微医云课堂</slot>
       <slot slot="right" name="right"></slot>
     </van-nav-bar>
-    <van-search placeholder="请输入关键词" shape="round" v-model="value"/>
+    <van-search placeholder="请输入关键词" shape="round" v-model="value" />
   </div>
 </template>
 
@@ -21,7 +21,11 @@ export default {
   watch: {},
   created() {},
   mounted() {},
-  methods: {},
+  methods: {
+    onClickLeft() {
+      this.$router.go(-1);
+    }
+  },
   components: {}
 };
 </script>
